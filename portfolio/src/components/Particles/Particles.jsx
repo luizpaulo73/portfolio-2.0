24b4a00cff46b232a81f3f4e0089e0f79724fb0b
luizpaulo73 <luiz.paulo2005@hotmail.com>
2025-03-03 21:01:20 -0300

    Remocao de js nao usado

--- portfolio/src/components/Particles/Particles.jsx
+++ portfolio/src/components/Particles/Particles.jsx
@@ -2,49 +2,17 @@
 
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
-
-
+import { loadSlim } from "@tsparticles/slim";
 
 const ParticlesComponent = (props) => {
 
   const [init, setInit] = useState(false);
-  // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
       await loadSlim(engine);
-      //await loadBasic(engine);
     }).then(() => {
       setInit(true);
     });
-  }, []);
-
-  const [particleNumber, setParticleNumber] = useState(getParticleNumber());
-
-  function getParticleNumber() {
-    return window.innerWidth < 768 ? 6 : 10;
-}
-
-// Hook para atualizar o número de partículas quando a tela for redimensionada
-  useEffect(() => {
-      function handleResize() {
-          setParticleNumber(getParticleNumber());
-      }
-
-      window.addEventListener("resize", handleResize);
-
-      // Limpa o event listener quando o componente for desmontado
-      return () => {
-          window.removeEventListener("resize", handleResize);
-      };
   }, []);
 
   const particlesLoaded = (container) => {
